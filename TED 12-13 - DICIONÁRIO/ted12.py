553fb6cdceee70540a3bf80d1e71ecf588e9eb57
Edilson Quirino Do Nascimento Júnior <95941206+edilsonjrcode@users.noreply.github.com>
2022-05-13 13:35:54 -0300

    new file

---
+++ TED 12-13 - DICIONÁRIO/ted12.py
@@ -0,0 +1,15 @@
+print('\033[1;31m''-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n')
+print('\033[1;31m'"GLOSSÁRIO DE INTRODUÇÃO A PROGRAMAÇÃO\n"'\033[0;30m')
+
+glossario = {'Python': ['Linguagem de programação', 'Linguagem de alto nível'],
+             'Visual Studio Code': ['Ambiente de Desenvolvimento Integrado', 'Editor de Código', 'Desenvolvido pela Microsoft'],
+             'Variável': ['Retém valores', 'Possui identificadores'],
+             'Algoritmo': ['Sequencia finita de ações', 'Visam obter uma solução'],
+             'Fluxograma': ['Tipo de diagrama', 'Representa processos', 'Possui figuras geométricas']}
+
+print('\033[1;31m''Algoritmo:''\033[1;30m', glossario['Algoritmo'])
+print('\033[1;31m''\nFluxograma:''\033[1;30m', glossario['Fluxograma'])
+print('\033[1;31m''\nVariável:''\033[1;30m', glossario['Variável'])
+print('\033[1;31m''\nVisual Studio Code:''\033[1;30m', glossario['Visual Studio Code'])
+print('\033[1;31m''\nPython:''\033[1;30m', glossario['Python'])
+print('\n\033[1;31m''-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-')
